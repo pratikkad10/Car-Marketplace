e9768a2154ad3c14bc3a046a4e12027d74ef55d6
pratik kad <pratikkad22@gmail.com>
2025-05-01 12:12:34 +0530

    roles added in user model!

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -17,8 +17,8 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["user", "admin"],
-      default: "user"
+      enum: ["buyer","seller", "admin"],
+      default: "buyer"
     },
     isVerified: {
       type: Boolean,
